c8b451738a12c97dda8e2cbce06f84c0d10b9bfc
roseyildiztas <roseyildiztas@gmail.com>
2022-01-16 07:08:52 +0000

    Handfull codes are added to Create_new_repository_cli.py

--- Create_new_repository_cli.py
+++ Create_new_repository_cli.py
@@ -14,6 +14,9 @@ git push -u origin master
 git remote add origin git@github.com:username/<reponame>.git
 git push -u origin master
 
+NOte: 1-Make sure you install git to your VM
+      2- add ssh-public key to GIthub account! 
+
 usuful codes
 1- git status
 
@@ -28,7 +31,7 @@ create a new branch with the name <branch>.
     Display the entire commit history using the default format.
 For customization see additional options.
 
-5- git init <directory>
+5- git init <directory> (-b main)
     Create empty Git repo in specified directory. Run with no
 arguments to initialize the current directory as a git repository.
 git commit
@@ -45,5 +48,22 @@ immediately merge it into the local copy
     Push the branch to <remote>, along with necessary commits and
 objects. Creates named branch in the remote repo if it doesn’t exist.
 
+9- git clone ssh LINk from repo/file
+    + $ git clone git@github.com:UserName/FileNAme.git (ssh LINk from repo)
+    + $ cd FileOrRepoName(jump in folder)
+    + $ git pull (for latest changes)
+
+10- Changing Branch amoung the existing branches in CLI
+    + $ git checkout BranchNameorMain
+
+11- git checkout -b "BranchName" # (that want to call) creating a new branch CLI
+  + $ git add fileName
+  + $ git commit-m "Explanation of changes"
+  + $ git push -u origin "BranchName" or main 
+
+
+12- git init:
+     Initialize the local directory as a Git repository.
+   + $ git init -b main
 
 usufulpage:https://www.atlassian.com/git/tutorials/atlassian-git-cheatsheet
